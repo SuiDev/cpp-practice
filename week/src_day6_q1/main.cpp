bf7d4cff8613a97c9e0039b191a374b436d889b6
sui <sui.ryo2102.tech@gmail.com>
2022-07-12 12:57:42 +0900

    day6

---
+++ week/src_day6_q1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "airplane.h"
+#include "fighter.h"
+ 
+using namespace std;
+ 
+int main(){
+    Fighter f;
+    Airplane a;
+    a.fly();
+    f.fly();
+    f.fight();
+    return 0;
+}
---
+++ week/src_day6_q2/newcalc.cpp
@@ -0,0 +1,11 @@
+#include "fundcalc.h"
+#include "newcalc.h"
+
+double NewCalc::mul()
+{
+    return m_number1 * m_number2;
+}
+double NewCalc::div()
+{
+    return m_number1 / m_number2;
+}
